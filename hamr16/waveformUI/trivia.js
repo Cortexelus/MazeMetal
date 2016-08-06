25b4292d06cf003d691b49b5dfe65030587287f7
ZVK <drawordoodle@gmail.com>
2016-08-06 15:15:09 -0400

    wavesurferUI first commit

---
+++ hamr16/waveformUI/trivia.js
@@ -0,0 +1,58 @@
+var GLOBAL_ACTIONS = {
+    'play': function () {
+        wavesurfer.playPause();
+    },
+
+    'back': function () {
+        wavesurfer.skipBackward();
+    },
+
+    'forth': function () {
+        wavesurfer.skipForward();
+    },
+
+    'toggle-mute': function () {
+        wavesurfer.toggleMute();
+    }
+};
+
+
+// Bind actions to buttons and keypresses
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('keydown', function (e) {
+        var map = {
+            32: 'play',       // space
+            37: 'back',       // left
+            39: 'forth'       // right
+        };
+        var action = map[e.keyCode];
+        if (action in GLOBAL_ACTIONS) {
+            if (document == e.target || document.body == e.target) {
+                e.preventDefault();
+            }
+            GLOBAL_ACTIONS[action](e);
+        }
+    });
+
+    [].forEach.call(document.querySelectorAll('[data-action]'), function (el) {
+        el.addEventListener('click', function (e) {
+            var action = e.currentTarget.dataset.action;
+            if (action in GLOBAL_ACTIONS) {
+                e.preventDefault();
+                GLOBAL_ACTIONS[action](e);
+            }
+        });
+    });
+});
+
+
+// Misc
+document.addEventListener('DOMContentLoaded', function () {
+    // Web Audio not supported
+    if (!window.AudioContext && !window.webkitAudioContext) {
+        var demo = document.querySelector('#demo');
+        if (demo) {
+            demo.innerHTML = 'Your browser does not run the Web Audio API';
+        }
+    }
+});
